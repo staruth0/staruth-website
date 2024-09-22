3208a2ebfe7d496628f08f943405633b5b9fee23
JIM440 <takemjim43@gmail.com>
2024-09-22 11:05:46 +0100

    adjustment to home page and addition of other sections

---
+++ frontend/src/pages/home/components/what_we_care_about/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import WhatWeCareAboutCard from './WhatWeCareAboutCard';
+import './WhatWeCareAbout.css';
+import quality from '../../../../assets/images/quality.png';
+import time from '../../../../assets/images/time.png';
+import budget from '../../../../assets/images/budget.png';
+
+const HomeWhatWeCareAbout = () => {
+  const items = [
+    {
+      id: 1,
+      img: quality,
+      title: 'Quality',
+      description: 'lorem20',
+    },
+    {
+      id: 2,
+      img: time,
+      title: 'Time',
+      description: 'lorem20',
+    },
+    {
+      id: 3,
+      img: budget,
+      title: 'Budget',
+      description: 'lorem20',
+    },
+  ];
+  return (
+    <div className="container home-what-we-care-about-section">
+      <h1>
+        When you are looking to grow, we are the 'A' team right by your side
+      </h1>
+      <p>For your projects, we at Staruth care about</p>
+      <div className="home-what-we-care-about-items">
+        {items.map((item) => (
+          <WhatWeCareAboutCard item={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomeWhatWeCareAbout;
