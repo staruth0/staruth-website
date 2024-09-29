9203934e6ba041ce4dff15a2a3bb946f4e3231e7
Akenji <akenjifaith1@gmail.com>
2024-09-29 20:07:56 +0100

    Add media queries

--- frontend/src/pages/teamOnClick/components/hero_section/hero.jsx
+++ frontend/src/pages/teamOnClick/components/hero_section/hero.jsx
@@ -6,41 +6,35 @@ import github_logo from '../../../../assets/icons/github_1.svg';
 import team_onclick_hero_image from '../../../../assets/images/team_onclick_hero_image.png';
 import linkedin_logo from '../../../../assets/icons/linkedin_1.svg';
 
-const TeamOnClickHeroComponent = () => {
+const TeamOnClickHeroComponent = ({member}) => {
   return (
     <div className="team-onclick-hero-section">
       <div className="team-onclick-hero-section-left">
         <header>
-          <h1>Akenji sirri</h1>
-          <h3>Frontend Developer</h3>
+          <h1>{member.name}</h1>
+          <h3>{member.title}</h3>
         </header>
         <p>
-          I am a sooo Lorem ipsum dolor sit amet consectetur. Quis orci ses
-          neque rhoncus. Integer commodo Lorem ipsum dolor sit amet consepsum
-          dolor sit amet consectetur. Quis orci sed fesi egestas neque rhoncus.
-          Integer commodo Lorem ipsum dolor sit amet consctetur. epsum dolor sit
-          amet consectetur. Quis orci sed fesi egestas epsum dolor sit amet
-          consectetur. Quis orci sed fesi egestas neque rhoncus. Integer commodo
-          Lorem ipsum dolor sit amet consectetur.
+{member.description}
         </p>
 
         <span>Let’s Connect:</span>
 
         <div className="team-onclick-hero-section-right-socials">
-          <a href="">
-            <img src={twitter_logo} alt="" />
+          <a href={member.socialMedia.twitter}>
+            <img src={twitter_logo} alt="noopener noreferrer" />
           </a>
-          <a href="">
-            <img src={github_logo} alt="" />
+          <a href={member.socialMedia.github}>
+            <img src={github_logo} alt="noopener noreferrer" />
           </a>
-          <a href="">
-            <img src={linkedin_logo} alt="" />
+          <a href={member.socialMedia.linkedin}>
+            <img src={linkedin_logo} alt="noopener noreferrer" />
           </a>
         </div>
       </div>
 
       <div className="team-onclick-hero-section-right">
-        <img src={team_onclick_hero_image} alt="" />
+        <img src={member.photoUrl} alt="" />
       </div>
     </div>
   );
