5bb592d9e279f9596a464b7638510f69e65b091f
Akenji <akenjifaith1@gmail.com>
2024-09-30 16:53:12 +0100

    adjust some more on responsiveness

--- frontend/src/pages/teamOnClick/components/hero_section/hero.jsx
+++ frontend/src/pages/teamOnClick/components/hero_section/hero.jsx
@@ -41,3 +41,55 @@ const TeamOnClickHeroComponent = ({member}) => {
 };
 
 export default TeamOnClickHeroComponent;
+
+
+
+// import React from 'react';
+// import './hero.css';
+
+// import twitter_logo from '../../../../assets/icons/prime_twitter_1.svg';
+// import github_logo from '../../../../assets/icons/github_1.svg';
+// import team_onclick_hero_image from '../../../../assets/images/team_onclick_hero_image.png';
+// import linkedin_logo from '../../../../assets/icons/linkedin_1.svg';
+
+// const TeamOnClickHeroComponent = () => {
+//   return (
+//     <div className="team-onclick-hero-section">
+//       <div className="team-onclick-hero-section-left">
+//         <header>
+//           <h1>Akenji sirri</h1>
+//           <h3>Frontend Developer</h3>
+//         </header>
+//         <p>
+//           I am a sooo Lorem ipsum dolor sit amet consectetur. Quis orci ses
+//           neque rhoncus. Integer commodo Lorem ipsum dolor sit amet consepsum
+//           dolor sit amet consectetur. Quis orci sed fesi egestas neque rhoncus.
+//           Integer commodo Lorem ipsum dolor sit amet consctetur. epsum dolor sit
+//           amet consectetur. Quis orci sed fesi egestas epsum dolor sit amet
+//           consectetur. Quis orci sed fesi egestas neque rhoncus. Integer commodo
+//           Lorem ipsum dolor sit amet consectetur.
+//         </p>
+
+//         <span>Let’s Connect:</span>
+
+//         <div className="team-onclick-hero-section-right-socials">
+//           <a href="">
+//             <img src={twitter_logo} alt="" />
+//           </a>
+//           <a href="">
+//             <img src={github_logo} alt="" />
+//           </a>
+//           <a href="">
+//             <img src={linkedin_logo} alt="" />
+//           </a>
+//         </div>
+//       </div>
+
+//       <div className="team-onclick-hero-section-right">
+//         <img src={team_onclick_hero_image} alt="" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TeamOnClickHeroComponent;
--- frontend/src/pages/teamOnClick/components/skills/skills.jsx
+++ frontend/src/pages/teamOnClick/components/skills/skills.jsx
@@ -36,3 +36,40 @@ const TeamOnClickSkillsComponent = ({member}) => {
 
 export default TeamOnClickSkillsComponent;
 
+
+
+// import React from "react";
+// import "./skills.css";
+
+// const TeamOnClickSkillsComponent = () => {
+//     return (
+//         <div className="team-on-click-skills-container">
+//             <div className="team-on-click-skills-container-tools">
+//                 <h1>Expertise and tools</h1>
+//                <span>Frontend Development:  </span>
+//                <p>ReactJs, JQuery, Boostrap, Tailwind</p>
+//             </div>
+
+//             <div className="team-on-click-skills-container-Qualifications">
+//                 <h1>Qualifications</h1>
+//                 <ul>
+//                     <li>BEng Computer Engineering</li>
+//                     <li>Google so so and so certification</li>
+//                     <li>Google so so and so certification</li>
+//                     <li>Google so so and so certification</li>
+//                 </ul>
+//             </div>
+
+//             <div className="team-on-click-skills-container-Achievements">
+//                 <h1>Key Achievements</h1>
+//                 <p>
+//                 rci sed fesi egestas neque rhoncus. Integer commodo Lorem ipsum 
+//                 dolor sit amet consctetur. Qepsum dolor sit amet consectetur. Quorci sed fesi egestas 
+//                 epsum dolor sit amet consectetur. Quis orci sed fesi egestas 
+//                 </p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default TeamOnClickSkillsComponent;
