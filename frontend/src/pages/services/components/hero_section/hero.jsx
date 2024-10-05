c5ae410432d0895636ada8eef2eb6768e1ae2a87
Kouete-kenneth <kouete678@gmail.com>
2024-10-05 10:08:49 +0100

    code at the level of servicecard gallery corrected

--- frontend/src/pages/services/components/hero_section/hero.jsx
+++ frontend/src/pages/services/components/hero_section/hero.jsx
@@ -5,7 +5,7 @@ const ServicesHeroSection = () => {
     return (
     <div className="service-hero-section-container">
         <p>
-        We critically design and build products that convert for your desired purpose. Every input we make is to realise progressive turnarounds in your work
+        We critically design and build products that convert for your desired purpose. Every input we make is to realize progressive turnarounds in your work
         </p>
     </div>
     );
