09977f8a751e3b639fe26f2b14bc358ccb5749ff
JIM440 <takemjim43@gmail.com>
2024-10-06 19:57:31 +0100

    style updates to home testimonial

--- frontend/src/pages/home/components/what_we_do/HomeServiceCard.jsx
+++ frontend/src/pages/home/components/what_we_do/HomeServiceCard.jsx
@@ -4,14 +4,12 @@ import { Link } from 'react-router-dom';
 const ServiceCard = ({ title, description, image }) => {
   return (
     <Link
+      to="/services"
       className="home-what-we-do-section-container-service-cards-container-cards"
       data-aos="fade-up"
     >
       <img src={image} alt={title} />
-      <div
-        to="/services"
-        className="home-what-we-do-section-container-service-cards-container-card-text-n-btn"
-      >
+      <div className="home-what-we-do-section-container-service-cards-container-card-text-n-btn">
         <div className="home-what-we-do-section-container-service-cards-container-card-text-n-btn-text">
           <h3>{title}</h3>
           <p>{description}</p>
