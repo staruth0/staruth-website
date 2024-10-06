f23d5401056be91cb343dbec9184aedba3b5f799
JIM440 <takemjim43@gmail.com>
2024-10-06 15:17:14 +0100

    adjusted styling in services page

--- frontend/src/pages/contact_us/contact.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './contact.css';
-
-const ContactPage = () => {
-  return <div className="contact-page">Contact Page</div>;
-};
-
-export default ContactPage;
--- frontend/src/pages/services/index.jsx
+++ frontend/src/pages/services/index.jsx
@@ -1,11 +1,11 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import ServicesHeroSection from './components/hero_section/hero';
 import OurServicesSection from './components/our_services/services';
-import './services.css';
-import axios from 'axios'
+import axios from 'axios';
+import Skeleton from './components/our_services/skeleton/Skeleton';
 
 const ServicesPage = () => {
-  const [services, setServices] = useState([])
+  const [services, setServices] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -15,7 +15,9 @@ const ServicesPage = () => {
   const getServices = async () => {
     setLoading(true);
     try {
-      const { data } = await axios.get("https://staruthwebsite-api.vercel.app/services/getServices");
+      const { data } = await axios.get(
+        'https://staruthwebsite-api.vercel.app/services/getServices'
+      );
       setServices(data);
     } catch (error) {
       console.error(error);
@@ -26,13 +28,11 @@ const ServicesPage = () => {
 
   return (
     <div className="service-page-container">
-      {loading ? <p>Loading...</p> : (
-        <>
-          <ServicesHeroSection />
-          <OurServicesSection services={services} />
-        </>
-      )}
+      <>
+        <ServicesHeroSection />
+        {loading ? <Skeleton /> : <OurServicesSection services={services} />}
+      </>
     </div>
   );
-}
-export default ServicesPage
+};
+export default ServicesPage;
